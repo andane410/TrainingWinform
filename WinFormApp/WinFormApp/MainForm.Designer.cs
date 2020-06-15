0ab121b2be949df811fde4a66df39e8985cf33b5
andane410 <andane2naver.com>
2020-06-15 17:36:13 +0900

    WinForm스터디

---
+++ WinFormApp/WinFormApp/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WinFormApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnMessage1 = new System.Windows.Forms.Button();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtCurrentDate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnMessage1
+            // 
+            this.BtnMessage1.BackColor = System.Drawing.SystemColors.Info;
+            this.BtnMessage1.Font = new System.Drawing.Font("맑은 고딕", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnMessage1.Location = new System.Drawing.Point(613, 382);
+            this.BtnMessage1.Name = "BtnMessage1";
+            this.BtnMessage1.Size = new System.Drawing.Size(175, 57);
+            this.BtnMessage1.TabIndex = 0;
+            this.BtnMessage1.Text = "현재 시간";
+            this.BtnMessage1.UseVisualStyleBackColor = false;
+            this.BtnMessage1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "현재 시간 : ";
+            // 
+            // TxtCurrentDate
+            // 
+            this.TxtCurrentDate.Location = new System.Drawing.Point(105, 49);
+            this.TxtCurrentDate.Name = "TxtCurrentDate";
+            this.TxtCurrentDate.Size = new System.Drawing.Size(231, 25);
+            this.TxtCurrentDate.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 451);
+            this.Controls.Add(this.TxtCurrentDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnMessage1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnMessage1;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtCurrentDate;
+    }
+}
+
---
+++ WinFormApp/WinFormApp/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Timer now = new Timer();
+            TxtCurrentDate.Text = now.ToString();
+        }
+    }
+}
